d7bd406a3791061f13ed146028ea80aaa19daafb
David Medinets <david.medinets@gmail.com>
2020-03-03 15:46:31 -0500

    get centos and ubuntu command-line

---
+++ get-bash-shell-centos.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+RANDOMIZER=$(uuid | cut -b-5)
+POD_NAME="bash-shell-$RANDOMIZER"
+IMAGE=centos
+
+cat <<EOF > yaml/shell-demo.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: $POD_NAME
+spec:
+  containers:
+  - name: $POD_NAME
+    image: $IMAGE
+    command: ["/bin/bash"]
+    args: ["-c", "while true; do date; sleep 5; done"]
+  hostNetwork: true
+  dnsPolicy: Default
+  restartPolicy: Never
+EOF
+
+kubectl apply -f yaml/shell-demo.yaml
+
+echo "---------------------------------"
+echo "| Press ^C when pod is running. |"
+echo "---------------------------------"
+
+kubectl get pod $POD_NAME -w
+
+echo
+
+kubectl exec -it $POD_NAME -- /bin/bash
+
+kubectl delete pod $POD_NAME
